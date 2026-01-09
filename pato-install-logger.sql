930b1a7f2b859604558a3a3b6b2e779655057d86
Gert-Jan Paulissen <12508981+gpaulissen@users.noreply.github.com>
2026-01-09 14:59:33 +0100

    Update pato-install-logger.sql

--- pato-install-logger.sql
+++ pato-install-logger.sql
@@ -1,14 +1,12 @@
-/*
-create user LOGGER_USER no authentication default tablespace DATA temporary tablespace TEMP
-/
-alter user LOGGER_USER quota unlimited on DATA
-/
-grant connect,create view, create job, create table, create sequence, create trigger, create procedure, create any context to LOGGER_USER
-/
-*/
-
 declare
   l_git_repo_index pato_cloud_repo.git_repo_index_t;
+  l_files_to_install constant sys.odcivarchar2list :=
+    sys.odcivarchar2list
+    ( 'scripts/create_user-no-sqlplus.sql'
+    , 'install/logger_install-no-sqlplus.sql'
+    , 'scripts/grant_logger_to_user-no-sqlplus.sql'
+    , 'scripts/create_logger_synonyms-no-sqlplus.sql'
+    );
 begin
   -- first call must be to paulissoft/oracle-tools branch development
   l_git_repo_index := 
@@ -24,30 +22,27 @@ begin
     , p_repo_name => 'Logger'
     , p_branch_name => 'feature/#1'
     );
-  pato_cloud_repo.install_file
-  ( p_git_repo_index => l_git_repo_index
-  , p_file_path => 'scripts/create_user-no-sqlplus.sql'
-  , p_stop_on_error => true
-  , p_schema => 'ADMIN'
-  );
-  pato_cloud_repo.install_file
-  ( p_git_repo_index => l_git_repo_index
-  , p_file_path => 'install/logger_install-no-sqlplus.sql'
-  , p_stop_on_error => true
-  , p_schema => 'LOGGER_USER'
-  );
-  pato_cloud_repo.install_file
-  ( p_git_repo_index => l_git_repo_index
-  , p_file_path => 'scripts/grant_logger_to_user-no-sqlplus.sql'
-  , p_stop_on_error => true
-  , p_schema => 'LOGGER_USER'
-  );
-  pato_cloud_repo.install_file
-  ( p_git_repo_index => l_git_repo_index
-  , p_file_path => 'scripts/create_logger_synonyms-no-sqlplus.sql'
-  , p_stop_on_error => true
-  , p_schema => 'LOGGER_USER'
-  );
+  for i_idx in l_files_to_install.first .. l_files_to_install.last
+  loop
+    begin
+      pato_cloud_repo.install_file
+      ( p_git_repo_index => l_git_repo_index
+      , p_file_path => l_files_to_install(i_idx)
+      , p_stop_on_error => true
+      , p_schema => case i_idx
+                      when l_files_to_install.first
+                      then 'ADMIN'
+                      when l_files_to_install.last
+                      then 'ORACLE_TOOLS'
+                      else 'LOGGER_USER'
+                    end
+      );
+    exception
+      when others
+      then
+        raise_application_error(-20000, l_files_to_install(i_idx), true);
+    end;
+  end loop;
   -- End of installing paulissoft/Logger
 end;
 /
