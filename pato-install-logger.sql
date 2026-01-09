4205976b8df6a43907260f972e7a35af3cc3777a
Gert-Jan Paulissen <12508981+gpaulissen@users.noreply.github.com>
2026-01-09 14:53:08 +0100

    Save point.

--- pato-install-logger.sql
+++ pato-install-logger.sql
@@ -1,3 +1,12 @@
+/*
+create user LOGGER_USER no authentication default tablespace DATA temporary tablespace TEMP
+/
+alter user LOGGER_USER quota unlimited on DATA
+/
+grant connect,create view, create job, create table, create sequence, create trigger, create procedure, create any context to LOGGER_USER
+/
+*/
+
 declare
   l_git_repo_index pato_cloud_repo.git_repo_index_t;
 begin
@@ -18,25 +27,25 @@ begin
   pato_cloud_repo.install_file
   ( p_git_repo_index => l_git_repo_index
   , p_file_path => 'scripts/create_user-no-sqlplus.sql'
-  , p_stop_on_error => false
+  , p_stop_on_error => true
   , p_schema => 'ADMIN'
   );
   pato_cloud_repo.install_file
   ( p_git_repo_index => l_git_repo_index
   , p_file_path => 'install/logger_install-no-sqlplus.sql'
-  , p_stop_on_error => false
+  , p_stop_on_error => true
   , p_schema => 'LOGGER_USER'
   );
   pato_cloud_repo.install_file
   ( p_git_repo_index => l_git_repo_index
   , p_file_path => 'scripts/grant_logger_to_user-no-sqlplus.sql'
-  , p_stop_on_error => false
+  , p_stop_on_error => true
   , p_schema => 'LOGGER_USER'
   );
   pato_cloud_repo.install_file
   ( p_git_repo_index => l_git_repo_index
   , p_file_path => 'scripts/create_logger_synonyms-no-sqlplus.sql'
-  , p_stop_on_error => false
+  , p_stop_on_error => true
   , p_schema => 'LOGGER_USER'
   );
   -- End of installing paulissoft/Logger
--- scripts/create_user-no-sqlplus.sql
+++ scripts/create_user-no-sqlplus.sql
@@ -2,6 +2,5 @@ begin
   execute immediate 'create user LOGGER_USER no authentication default tablespace DATA temporary tablespace TEMP';
   execute immediate 'alter user LOGGER_USER quota unlimited on DATA';
   execute immediate 'grant connect,create view, create job, create table, create sequence, create trigger, create procedure, create any context to LOGGER_USER';
-  execute immediate 'alter user LOGGER_USER lock account';
 end;
 /
