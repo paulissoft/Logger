404e9c7a8387e92b206fbbd4398974ed761462f6
Gert-Jan Paulissen <12508981+gpaulissen@users.noreply.github.com>
2026-01-09 16:46:58 +0100

    Save point.

--- scripts/grant_logger_to_user-no-sqlplus.sql
+++ scripts/grant_logger_to_user-no-sqlplus.sql
@@ -8,19 +8,19 @@ define to_user = '&1' -- This is the user to grant the permissions to
 whenever sqlerror exit sql.sqlcode
 */
 
-grant execute on logger to oracle_tools
+grant execute on logger_user.logger to oracle_tools
 /
-grant select, delete on logger_logs to oracle_tools
+grant select, delete on logger_user.logger_logs to oracle_tools
 /
-grant select on logger_logs_apex_items to oracle_tools
+grant select on logger_user.logger_logs_apex_items to oracle_tools
 /
-grant select, update on logger_prefs to oracle_tools
+grant select, update on logger_user.logger_prefs to oracle_tools
 /
-grant select on logger_prefs_by_client_id to oracle_tools
+grant select on logger_user.logger_prefs_by_client_id to oracle_tools
 /
-grant select on logger_logs_5_min to oracle_tools
+grant select on logger_user.logger_logs_5_min to oracle_tools
 /
-grant select on logger_logs_60_min to oracle_tools
+grant select on logger_user.logger_logs_60_min to oracle_tools
 /
-grant select on logger_logs_terse to oracle_tools
+grant select on logger_user.logger_logs_terse to oracle_tools
 /
--- scripts/grant_logger_to_user.sql
+++ scripts/grant_logger_to_user.sql
@@ -1,17 +1,17 @@
--- Grants privileges for logger objects from current user to a defined user
+-- Grants privileges for logger objects from a user to another user
 
 
 -- Parameters
-define to_user = '&1' -- This is the user to grant the permissions to
-
+define from_user = '&1' -- This is the user to grant the permissions from
+define to_user = '&2' -- This is the user to grant the permissions to
 
 whenever sqlerror exit sql.sqlcode
 
-grant execute on logger to &to_user;
-grant select, delete on logger_logs to &to_user;
-grant select on logger_logs_apex_items to &to_user;
-grant select, update on logger_prefs to &to_user;
-grant select on logger_prefs_by_client_id to &to_user;
-grant select on logger_logs_5_min to &to_user;
-grant select on logger_logs_60_min to &to_user;
-grant select on logger_logs_terse to &to_user;
+grant execute on &from_user..logger to &to_user;
+grant select, delete on &from_user..logger_logs to &to_user;
+grant select on &from_user..logger_logs_apex_items to &to_user;
+grant select, update on &from_user..logger_prefs to &to_user;
+grant select on &from_user..logger_prefs_by_client_id to &to_user;
+grant select on &from_user..logger_logs_5_min to &to_user;
+grant select on &from_user..logger_logs_60_min to &to_user;
+grant select on &from_user..logger_logs_terse to &to_user;
