94ccac5e830d1cb0c394df96622acbd7b4954d66
Gert-Jan Paulissen <12508981+gpaulissen@users.noreply.github.com>
2026-01-09 15:00:59 +0100

    Update create_user-no-sqlplus.sql

--- scripts/create_user-no-sqlplus.sql
+++ scripts/create_user-no-sqlplus.sql
@@ -1,6 +1,6 @@
-begin
-  execute immediate 'create user LOGGER_USER no authentication default tablespace DATA temporary tablespace TEMP';
-  execute immediate 'alter user LOGGER_USER quota unlimited on DATA';
-  execute immediate 'grant connect,create view, create job, create table, create sequence, create trigger, create procedure, create any context to LOGGER_USER';
-end;
+create user LOGGER_USER no authentication default tablespace DATA temporary tablespace TEMP
+/
+alter user LOGGER_USER quota unlimited on DATA
+/
+grant connect,create view, create job, create table, create sequence, create trigger, create procedure, create any context to LOGGER_USER
 /
